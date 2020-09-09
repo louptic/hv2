7d1c23e101f56c098a4b83157b29e6bc9ce66504
GitHub Package Updater <action@github.com>
2020-09-09 16:46:12 +0000

    Updated Formula

--- Formula/Api.rb
+++ Formula/Api.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Api < Formula
   desc "The Optic CLI"
   homepage "https://github.com/opticdev/optic"
-  url "https://registry.npmjs.org/@useoptic/cli/-/cli-8.3.3.tgz"
-  version "8.3.3"
-  sha256 "f8415ac4975fe33d104aa18e92f78fed949110ed9f38c390006bac77d5cb6f41"
+  url "https://registry.npmjs.org/@useoptic/cli/-/cli-8.3.4.tgz"
+  version "8.3.4"
+  sha256 "c617f959b7faf1ca2222b021993ed0b7d99353023bfe96bf16cef3ae199725a8"
 
   depends_on "node"
 
